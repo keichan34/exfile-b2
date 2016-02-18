76c54a518a8cc5d9b4114aa578cce9b7f8332e6b
Keita Kobayashi <keita@kbys.me>
2016-02-18 23:23:41 +0900

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExfileB2.Mixfile do
   def project do
     [
       app: :exfile_b2,
-      version: "0.0.3",
+      version: "0.1.0",
       elixir: "~> 1.2.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,

6f40f7b9082ee97c1c390a99b46bb63f78322a14
Keitaroh Kobayashi <keita@kbys.me>
2016-05-02 16:01:16 +0900

    Move test files

--- test/exfile_b2_test.exs
+++ test/exfile_b2_test.exs
@@ -1,8 +1,3 @@
 defmodule ExfileB2Test do
-  use Exfile.BackendTest, {
-    ExfileB2.Backend,
-    account_id: "valid_account_id",
-    application_key: "valid_application_key",
-    bucket: "bucket"
-  }
+  use ExUnit.Case
 end
